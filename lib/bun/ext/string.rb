480ac064a9b0e68e5879fa0f987323baa5d05ce0
k1LoW <k1lowxb@gmail.com>
2015-10-04 21:54:43 +0900

    Add '.'

--- lib/bun/ext/string.rb
+++ lib/bun/ext/string.rb
@@ -1,7 +1,7 @@
 class String
   def is_part_of_bun?
     return true if self.match(/\A\d+\z/)
-    return true if [','].include?(self.to_s)
+    return true if [',', '.'].include?(self.to_s)
     !self.ascii_only?
   end
 end
--- spec/bun_spec.rb
+++ spec/bun_spec.rb
@@ -4,12 +4,6 @@ require 'pp'
 
 TESTSETS = File.dirname(__FILE__) + "/testsets/testset.yml";
 
-describe Bun do
-  it 'has a version number' do
-    expect(Bun::VERSION).not_to be nil
-  end
-end
-
 describe Bun::Exctractor do
   YAML.load_file(TESTSETS).each do |e|
     it 'extract_line( ' + e['target'] + ' )'do
