5fab713af887d695479deaf02a1b5ddf4792df50
k1LoW <k1lowxb@gmail.com>
2015-10-06 21:37:14 +0900

    wait refactor....

--- lib/bun/cli.rb
+++ lib/bun/cli.rb
@@ -11,31 +11,34 @@ module Bun
       files = files[0] if files.is_a?(Array) && files[0].is_a?(Array) # @dirty
       files = [files] if !files.nil? && files.is_a?(String) # @dirty
       # files
-      files.each do |file|
-        file_path = file.to_s.gsub(/\e\[\d{1,3}[mK]/, '')
-        File.open(file_path, 'r') do |f|
-          f.readlines.each_with_index do |line, i|
-            paragraphs = Bun::Exctractor.extract_line(line)
-            paragraphs.each do |p|
-              if options[:number]
-                puts file_path + ':' + i.to_s + ":\t" + p
-              else
-                puts file_path + ":\t" + p
-              end
-            end unless paragraphs.count == 0
-          end
-        end if File.exist?(file_path)
-      end
-      # STDIN
-      STDIN.each_with_index do |line, i|
-        paragraphs = Bun::Exctractor.extract_line(line)
-        paragraphs.each do |p|
-          if options[:number]
-            puts i.to_s + ":\t" + p
-          else
-            puts p
-          end
-        end unless paragraphs.count == 0
+      if !files.empty?
+        files.each do |file|
+          file_path = file.to_s.gsub(/\e\[\d{1,3}[mK]/, '')
+          File.open(file_path, 'r') do |f|
+            f.readlines.each_with_index do |line, i|
+              paragraphs = Bun::Exctractor.extract_line(line)
+              paragraphs.each do |p|
+                if options[:number]
+                  puts file_path + ':' + i.to_s + ":\t" + p
+                else
+                  puts file_path + ":\t" + p
+                end
+              end unless paragraphs.count == 0
+            end
+          end if File.exist?(file_path)
+        end
+      else
+        # STDIN
+        STDIN.each_with_index do |line, i|
+          paragraphs = Bun::Exctractor.extract_line(line)
+          paragraphs.each do |p|
+            if options[:number]
+              puts i.to_s + ":\t" + p
+            else
+              puts p
+            end
+          end unless paragraphs.count == 0
+        end
       end
     end
 
