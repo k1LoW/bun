6ceedbed14ffa8640868bdb6e80c71bd6285da32
k1LoW <k1lowxb@gmail.com>
2015-10-07 13:50:01 +0900

    battle against rubocop

--- lib/bun/cli.rb
+++ lib/bun/cli.rb
@@ -13,37 +13,38 @@ module Bun
       # files
       if !files.empty?
         files.each do |file|
-          file_path = file.to_s.gsub(/\e\[\d{1,3}[mK]/, '')
+          file_path = file.to_s.gsub(/\e\[\d{1,3}[mK]/, '') # strinp console character
           File.open(file_path, 'r') do |f|
             f.readlines.each_with_index do |line, i|
-              paragraphs = Bun::Exctractor.extract_line(line)
-              paragraphs.each do |p|
-                if options[:number]
-                  puts file_path + ':' + i.to_s + ":\t" + p
-                else
-                  puts file_path + ":\t" + p
-                end
-              end unless paragraphs.count == 0
+              puts_line(file_path, i, line).strip
             end
           end if File.exist?(file_path)
         end
       else
         # STDIN
         STDIN.each_with_index do |line, i|
-          paragraphs = Bun::Exctractor.extract_line(line)
-          paragraphs.each do |p|
-            if options[:number]
-              puts i.to_s + ":\t" + p
-            else
-              puts p
-            end
-          end unless paragraphs.count == 0
+          puts_line(nil, i, line)
         end
       end
     end
 
     def method_missing(*files)
       extract(files)
+    end
+
+    private
+
+    def puts_line(prefix, i, line)
+      prefix += ':' unless prefix.nil?
+      paragraphs = Bun::Exctractor.extract_line(line)
+      paragraphs.each do |p|
+        if options[:number]
+          puts prefix.to_s + i.to_s + ":\t" + p
+        else
+          l = prefix.to_s + "\t" + p
+          puts l.strip
+        end
+      end unless paragraphs.count == 0
     end
   end
 end
